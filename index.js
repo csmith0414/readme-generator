791c362eb9e43e3e596b451ce606576069bc65ca
Chad Smith <cds021@yahoo.com>
2023-08-31 14:09:09 -0400

    updated generateMarkdown.js file

--- index.js
+++ index.js
@@ -64,25 +64,17 @@ function writeToFile (fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-  try {
-
-    // Prompt Inquirer questions
-    const userResponses = inquirer.prompt(questions);
-
-    // Call GitHub api for user info
-    const userInfo = api.getUser(userResponses);
-
-    // Pass Inquirer userResponses and GitHub userInfo to generateMarkdown
-    const markdown = generateMarkdown(userResponses, userInfo);
-    console.log(markdown);
-
-    // Write markdown to file
-     writeFileAsync('ExampleREADME.md', markdown);
-
-} catch (error) {
-    console.log(error);
-}
-};
+  return inquirer.prompt(questions);
+ };
 
 // Function call to initialize app
-init();
+init()
+.then(userInput => {
+  return generateMarkdown(userInput);
+})
+.then(readmeInfo => {
+  return writeToFile(readmeInfo);
+})
+.catch(err => {
+  console.log(err);
+});
